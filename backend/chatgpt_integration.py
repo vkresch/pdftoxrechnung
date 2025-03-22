44dfc235cd8f0eb3348c3363c920a79ae67cbe36
Viktor Kreschenski <viktor.kreschenski@gmail.com>
2025-03-23 00:49:10 +0100

    Handle captcha

--- backend/chatgpt_integration.py
+++ backend/chatgpt_integration.py
@@ -20,13 +20,15 @@ def process_with_chatgpt(pdf_text, test=False):
     logging.info(f"Starting ChatGPT extraction process ...")
     with SB(
         uc=True,
-        ad_block=True,
         test=test,
         # user_data_dir="./user_data",  # Reuse Chrome profile to persist login
     ) as sb:
         url = "https://chatgpt.com/"
-        # sb.uc_open_with_reconnect(url)
         sb.activate_cdp_mode(url)
+        sb.sleep(1)
+        sb.uc_gui_click_captcha()
+        sb.sleep(1)
+        sb.uc_gui_handle_captcha()
         sb.sleep(1)
         sb.click_if_visible('button[aria-label="Close dialog"]')
         sb.wait_for_element_visible("#prompt-textarea", timeout=10)
