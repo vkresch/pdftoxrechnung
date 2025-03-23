283c7e3620161eefd3cfbf3a871b27bf670712ed
Viktor Kreschenski <viktor.kreschenski@gmail.com>
2025-03-23 12:31:15 +0100

    Increase timeout for tests

--- backend/deepseek_integration.py
+++ backend/deepseek_integration.py
@@ -74,7 +74,7 @@ def process_with_deepseek(pdf_text, test=False):
 
         # Wait for response elements to load
         logging.info("Extracting pdf data into json ...")
-        sb.wait_for_element_visible("//div[@class='ds-flex _965abe9']", timeout=180)
+        sb.wait_for_element_visible("//div[@class='ds-flex _965abe9']", timeout=240)
 
         # response = sb.find_element(".ds-markdown--block p").text
         response = sb.find_element(".md-code-block pre").text
